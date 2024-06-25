aa4758c75705d6f991292f11fbf135cc00f0a045
Micaela Estabillo <micaela.estabillo@consensys.net>
2024-06-25 15:45:50 -0700

    chore: read bridge chains from feature flags and add selectors

--- app/scripts/controllers/bridge.test.ts
+++ app/scripts/controllers/bridge.test.ts
@@ -1,9 +1,14 @@
+import nock from 'nock';
+import { CHAIN_IDS } from '../../../shared/constants/network';
+import { BRIDGE_API_BASE_URL } from '../../../shared/constants/bridge';
 import BridgeController from './bridge';
 
 const EMPTY_INIT_STATE = {
   bridgeState: {
     bridgeFeatureFlags: {
       extensionSupport: false,
+      srcNetworkAllowlist: [],
+      destNetworkAllowlist: [],
     },
   },
 };
@@ -12,16 +17,32 @@ describe('BridgeController', function () {
   let bridgeController: BridgeController;
 
   beforeAll(function () {
+    jest.clearAllMocks();
     bridgeController = new BridgeController();
+  });
+
+  beforeEach(() => {
+    nock(BRIDGE_API_BASE_URL)
+      .get('/getAllFeatureFlags')
+      .reply(200, {
+        'extension-support': true,
+        'src-network-allowlist': [10, 534352],
+        'dest-network-allowlist': [137, 42161],
+      });
   });
 
   it('constructor should setup correctly', function () {
     expect(bridgeController.store.getState()).toStrictEqual(EMPTY_INIT_STATE);
   });
 
-  it('setBridgeFeatureFlags should set the bridge feature flags', function () {
-    const featureFlagsResponse = { extensionSupport: true };
-    bridgeController.setBridgeFeatureFlags(featureFlagsResponse);
+  it('setBridgeFeatureFlags should set the bridge feature flags', async function () {
+    const featureFlagsResponse = {
+      extensionSupport: true,
+      destNetworkAllowlist: [CHAIN_IDS.POLYGON, CHAIN_IDS.ARBITRUM],
+      srcNetworkAllowlist: [CHAIN_IDS.OPTIMISM, CHAIN_IDS.SCROLL],
+    };
+
+    await bridgeController.setBridgeFeatureFlags();
     expect(
       bridgeController.store.getState().bridgeState.bridgeFeatureFlags,
     ).toStrictEqual(featureFlagsResponse);
